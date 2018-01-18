f89b8af666cc38afc4d36f2301d6247a97058f05
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2018-01-18 12:44:09 -0500

    JFoenix library + slider

---
+++ Jransit/Jransit/src/UserInterface/Main.java
@@ -0,0 +1,46 @@
+package UserInterface;
+	
+import com.jfoenix.controls.JFXSlider;
+import com.jfoenix.controls.JFXSlider.IndicatorPosition;
+
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			HBox slider = new HBox();
+			JFXSlider hor_left = new JFXSlider();
+			hor_left.setMinWidth(500);
+			JFXSlider hor_right = new JFXSlider();
+			hor_left.setMinWidth(500);
+			hor_left.setIndicatorPosition(IndicatorPosition.RIGHT);
+			JFXSlider ver_left = new JFXSlider();
+			ver_left.setMinHeight(500);
+			ver_left.setOrientation(Orientation.VERTICAL);
+			JFXSlider ver_right = new JFXSlider();
+			ver_right.setMinHeight(500);
+			ver_right.setOrientation(Orientation.VERTICAL);
+			ver_right.setIndicatorPosition(IndicatorPosition.RIGHT);
+			root.getChildren().add(slider);
+			slider.getChildren().addAll(hor_left, hor_right, ver_left, ver_right);
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
