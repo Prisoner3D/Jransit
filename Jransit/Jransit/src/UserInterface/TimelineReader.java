ab22ddc34daa0b8fadda5319329527b30c9ee791
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-01-18 13:07:54 -0500

    Fixed jar + TimelineReader

---
+++ Jransit/Jransit/src/UserInterface/TimelineReader.java
@@ -0,0 +1,31 @@
+package UserInterface;
+
+import com.jfoenix.controls.JFXSlider;
+import com.jfoenix.controls.JFXSlider.IndicatorPosition;
+
+import javafx.geometry.Orientation;
+
+public class TimelineReader {
+
+	public TimelineReader() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**TODO:
+	 * SLIDER
+	 * CSV Reader
+	 * Time component
+	 * etc..
+	 */
+	/**
+	 * hor_left.setMinWidth(500);
+			hor_left.setIndicatorPosition(IndicatorPosition.RIGHT);
+			JFXSlider ver_left = new JFXSlider();
+			ver_left.setMinHeight(500);
+			ver_left.setOrientation(Orientation.VERTICAL);
+			JFXSlider ver_right = new JFXSlider();
+			ver_right.setMinHeight(500);
+			ver_right.setOrientation(Orientation.VERTICAL);
+	 */
+
+}
