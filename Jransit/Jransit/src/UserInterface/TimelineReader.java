d2b5270a57796045f86417f3d65254149b3076d5
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-01-31 12:41:58 -0500

    slider

--- Jransit/Jransit/src/UserInterface/TimelineReader.java
+++ Jransit/Jransit/src/UserInterface/TimelineReader.java
@@ -1,16 +1,47 @@
 package UserInterface;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 import com.jfoenix.controls.JFXSlider;
 import com.jfoenix.controls.JFXSlider.IndicatorPosition;
 
 import javafx.geometry.Orientation;
 
 public class TimelineReader {
+		private CSVReader csv;
+		private DateFrom;
+		private DateTo;
+		private JFXSlider slider;
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+		LocalDateTime now = LocalDateTime.now();
 
-	public TimelineReader() {
-		// TODO Auto-generated constructor stub
+	public TimelineReader(CSVReader csv, JFXSlider slider) {
+		this.DateFrom = new Date(dtf.format(now));
+		this.csv = csv;
+		//this.DateTo = last row of csv reader history sort DESC
+		this.slider = slider;
 	}
 	
+    public sliderValues(double min, double max, double value) {
+        super(min, max, value);
+        initialize();
+    }
+    
+
+    public IndicatorPosition getIndicatorPosition() {
+        return indicatorPosition == null ? IndicatorPosition.LEFT : indicatorPosition.get();
+    }
+
+    public StyleableObjectProperty<IndicatorPosition> indicatorPositionProperty() {
+        return this.indicatorPosition;
+    }
+
+    public void setIndicatorPosition(IndicatorPosition pos) {
+        this.indicatorPosition.set(pos);
+    }
+    
 	/**TODO:
 	 * SLIDER
 	 * CSV Reader
