d603f8c976bf0fa955a39d09bfb1c5d69e192256
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-01-31 13:04:19 -0500

    pls no brek

--- Jransit/Jransit/src/UserInterface/MapOverlay.java
+++ Jransit/Jransit/src/UserInterface/MapOverlay.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -17,13 +18,21 @@ public class MapOverlay extends Application
 		Widget theWidget = newWidget("station","station description");
 		try 
 		{
+			BorderPane root = new BorderPane(); 
 			VBox widget = new VBox();
 			String title = theWidget.title;
 			String description = theWidget.description;
 			Text t = new Text(title);
 			Text d = new Text(description);
+			VBox widget1 = new VBox();
+			String title1 = theWidget.title;
+			String description1 = theWidget.description;
+			Text t1 = new Text(title1);
+			Text d1 = new Text(description1);
 			widget.getChildren().addAll(t,d);
-			Scene scene = new Scene(widget,1900,1000);
+			widget1.getChildren().addAll(t1,d1);
+			root.getChildren().addAll(widget,widget1);
+			Scene scene = new Scene(root,1900,1000);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} 
