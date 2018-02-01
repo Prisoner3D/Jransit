7867a8002ac8956b46de482cfcfba803083d2c78
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-02-01 13:01:12 -0500

    Change test trip_id

--- Jransit/Jransit/src/Test.java
+++ Jransit/Jransit/src/Test.java
@@ -5,8 +5,10 @@ public class Test {
 		String key = "e7ed4dd1445f127eb503c38630a5d3e0";
 		MTAApi mta = new MTAApi(key, TrainFeed.BLUE);
 		//System.out.println(mta.getTrains());
+		
 		//mta.getStations();
-		System.out.println(mta.getStopTimes("077450_E..S"));
+		//System.out.println(mta.getStopTimes("077450_E..S"));
+		System.out.println(mta.getArrivalTime("081100_E..S", "E01S"));
 		//System.out.println(mta.grabTrainInfo("077450_E..S"));
 	}
 }
