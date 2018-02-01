850eff8c69b4016b49f4ad7647ba8a1516b94723
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2018-02-01 12:11:15 -0500

    add transfer csv files

--- Jransit/Jransit/src/csv/Transfer.java
+++ Jransit/Jransit/src/csv/Transfer.java
@@ -1,15 +1,43 @@
 package csv;
 
 public class Transfer {
-	private String fromStopId;
-	private String toStopId;
-	private String transferType;
-	private String transferTime;
-	public Transfer(String fromStopId, String toStopId, String transferType, String transferTime) {
-		this.fromStopId = fromStopId;
-		this.toStopId = toStopId;
-		this.transferType = transferType;
-		this.transferTime = transferTime;
+	private final String fromStopID;
+	private final Entity entity;
+	private final String toStopID;
+	private final String transferType;
+	private final String minTransferTime;
+
+	public Transfer(Entity entity) {
+		this.fromStopID = entity.getPrimaryKey();
+		this.entity = entity;
+		this.toStopID = entity.getAttribute("to_stop_id");
+		this.transferType = entity.getAttribute("transfer_type");
+		this.minTransferTime = entity.getAttribute("min_transfer_time");
+	}
+	
+	public Entity getEntity() {
+		return entity;
+	}
+
+	public String getFromStopID() {
+		return fromStopID;
+	}
+
+	public String getToStopID() {
+		return toStopID;
+	}
+
+	public String getTransferType() {
+		return transferType;
+	}
+
+	public String getMinTransferTime() {
+		return minTransferTime;
+	}
+	
+	@Override
+	public String toString() {
+		return String.valueOf(entity.getAttributes());
 	}
 
 }
--- Jransit/Jransit/src/csv/TransferDb.java
+++ Jransit/Jransit/src/csv/TransferDb.java
@@ -1,6 +1,7 @@
 package csv;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransferDb {
@@ -8,11 +9,16 @@ public class TransferDb {
 	private List<String> CSVData;
 	
 	public TransferDb() {
-		this.csv = new CSVUtilities(new File("Jransit\\data\\stops.txt"));
+		this.csv = new CSVUtilities(new File("Jransit\\data\\transfers.txt"));
 		this.CSVData = this.csv.getCSVData();
 	}
 	
-	public List<Transfer> getTransfers() {
-		
+	public List<Transfer> getTransfers(String fromID) {
+		List<Entity> entities = csv.getAllEntities(1, fromID);
+		List<Transfer> transfers = new ArrayList<>();
+		for (Entity ent : entities) {
+			transfers.add(new Transfer(ent));
+		}
+		return transfers;
 	}
 }
