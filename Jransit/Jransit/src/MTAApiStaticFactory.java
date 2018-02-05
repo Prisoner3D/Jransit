708afbfaad8504db19c0c1ad23be56c8525774c7
Alex Feng <afeng5853@bths.edu>
2018-02-05 13:05:40 -0500

    Create MTAApiStaticFactory.java

---
+++ Jransit/Jransit/src/MTAApiStaticFactory.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MTAApiStaticFactory {
+	private static Map<TrainFeed, MTAApi> apis = new HashMap<>();
+	public static Map<TrainFeed, MTAApi> getApis() {
+		return apis;
+	}
+	private static final String key = "e7ed4dd1445f127eb503c38630a5d3e0";
+	static {		
+		try {
+			apis.put(TrainFeed.NUM_S, new MTAApi(key, TrainFeed.NUM_S));
+			apis.put(TrainFeed.BLUE, new MTAApi(key, TrainFeed.BLUE));
+			apis.put(TrainFeed.YELLOW, new MTAApi(key, TrainFeed.YELLOW));
+			apis.put(TrainFeed.ORANGE, new MTAApi(key, TrainFeed.ORANGE));
+			apis.put(TrainFeed.GRAY, new MTAApi(key, TrainFeed.GRAY));
+			apis.put(TrainFeed.GREEN, new MTAApi(key, TrainFeed.GREEN));
+			apis.put(TrainFeed.BROWN, new MTAApi(key, TrainFeed.BROWN));
+			apis.put(TrainFeed.SIR, new MTAApi(key, TrainFeed.SIR));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	private MTAApiStaticFactory() {
+		
+	}
+
+}
