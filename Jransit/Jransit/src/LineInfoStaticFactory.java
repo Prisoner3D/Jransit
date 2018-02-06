d46290ce7d4793fec8e57f5bbc450f85b3857ae4
Alex Feng <afeng5853@bths.edu>
2018-02-06 12:11:50 -0500

    Update LineInfoStaticFactory.java

--- Jransit/Jransit/src/LineInfoStaticFactory.java
+++ Jransit/Jransit/src/LineInfoStaticFactory.java
@@ -14,8 +14,12 @@ public class LineInfoStaticFactory {
 	private static final String GRAY = "L";
 	private static final String GREEN = "G";
 	private static final String BROWN = "JZ";
+	
+	public static List<LineInfo> getLines() {
+		return lineArray;
+	}
 	private static List<LineInfo> lineArray = new ArrayList<>();
-	public LineInfoStaticFactory() {
+	static {
 		Map<TrainFeed, MTAApi> apis = MTAApiStaticFactory.getApis();
 		for (Entry<TrainFeed, MTAApi> entry : apis.entrySet()) {
 			TrainFeed key = entry.getKey();
@@ -30,13 +34,55 @@ public class LineInfoStaticFactory {
 						}
 		    		}
 		    	case NUM_S:
-		    		for (int i = 0; i < LineInfoStaticFactory.BLUE.length(); i++) {
+		    		for (int i = 0; i < LineInfoStaticFactory.NUM_S.length(); i++) {
 		    			try {
-							lineArray.add(new LineInfo(api, LineInfoStaticFactory.BLUE.substring(i, i+1)));
+							lineArray.add(new LineInfo(api, LineInfoStaticFactory.NUM_S.substring(i, i+1)));
 						} catch (IOException e) {
 							e.printStackTrace();
 						}
 		    		}
+		    	case YELLOW:
+		    		for (int i = 0; i < LineInfoStaticFactory.YELLOW.length(); i++) {
+		    			try {
+							lineArray.add(new LineInfo(api, LineInfoStaticFactory.YELLOW.substring(i, i+1)));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+		    		}
+		    	case ORANGE:
+		    		for (int i = 0; i < LineInfoStaticFactory.ORANGE.length(); i++) {
+		    			try {
+							lineArray.add(new LineInfo(api, LineInfoStaticFactory.ORANGE.substring(i, i+1)));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+		    		}
+		    	case GRAY:
+		    		for (int i = 0; i < LineInfoStaticFactory.GRAY.length(); i++) {
+		    			try {
+							lineArray.add(new LineInfo(api, LineInfoStaticFactory.GRAY.substring(i, i+1)));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+		    		}
+		    	case GREEN:
+		    		for (int i = 0; i < LineInfoStaticFactory.GREEN.length(); i++) {
+		    			try {
+							lineArray.add(new LineInfo(api, LineInfoStaticFactory.GREEN.substring(i, i+1)));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+		    		}
+		    	case BROWN:
+		    		for (int i = 0; i < LineInfoStaticFactory.BROWN.length(); i++) {
+		    			try {
+							lineArray.add(new LineInfo(api, LineInfoStaticFactory.BROWN.substring(i, i+1)));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+		    		}
+				default:
+					break;
 		    }
 		}
 	}
