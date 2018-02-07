b89911e3421114a6d155a600eaa7e76a884b25ef
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2018-02-07 12:12:54 -0500

    Simplified StopsStaticFactory

--- Jransit/Jransit/src/csv/StopsStaticFactory.java
+++ Jransit/Jransit/src/csv/StopsStaticFactory.java
@@ -9,15 +9,13 @@ import java.util.List;
  */
 public class StopsStaticFactory {
 	private static List<Stop> stops;
-	
+	static {
+		stops = new StopDb().getAllStops();
+	}
 	public static List<Stop> getAllStops() {
-		if (stops == null)
-			stops = new StopDb().getAllStops();
 		return stops;
 	}
 	public static Stop getStop(String stopID) {
-		if (stops == null)
-			stops = new StopDb().getAllStops();
 		for (Stop stop : stops) {
 			if (stop.getStopID().equals(stopID)) {
 				return stop;
